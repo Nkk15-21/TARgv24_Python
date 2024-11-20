9d71080a571124d7f49239e9592af2ec6b31501d
opilane <opilane@HP-CZC2349HT9>
2024-11-20 16:01:35 +0200

    Ülesanne 2

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -1,1 +1,20 @@
-print ("Hello World!")
+#Ülessane 1
+print ("Tere tulemast!")
+nimi=input("Mis on sinu nimi?")
+print("Tere tulemast! Tervitan Sind ", nimi)
+print("Tere tulemast! Tervitan Sind "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind "+ nimi+" Sa oled ",vanus,"aastat vana")
+print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus}aastat vana")
+
+
+#Ülessane 2 
+vanus = 18
+eesnimi = "Jaak"
+pikkus = 16.5
+kas_käib_koolis = True
+print(type(vanus))
+print(type(eesnimi))
+print(type(pikkus))
+print(type(kas_käib_koolis))
+
