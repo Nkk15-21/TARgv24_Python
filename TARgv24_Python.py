1e6c6d7ee6832a08767306290c8fc4dcfe2b6b2c
opilane <opilane@HP-CZC2349HT9>
2024-11-20 16:45:24 +0200

    Всё хорошо

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -5,32 +5,33 @@ from random import * # * - ВСЕ ФУНКЦИИ, randint as rd переназв
 from math import * #pi kasutamiseks
 
 #Ülessane 1
-print ("Tere tulemast!")
-nimi=input("Mis on sinu nimi?").capitalize() #lower() nikita, upper() NIKITA, capitalize() - Nikita
-print("Tere tulemast! Tervitan Sind ", nimi)
-print("Tere tulemast! Tervitan Sind "+ nimi)
-vanus=int(input("Kui vana sa oled? "))
-print("Tere tulemast! Tervitan sind "+ nimi+" Sa oled ",vanus,"aastat vana")
-print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
+# print ("Tere tulemast!")
+# nimi=input("Mis on sinu nimi?").capitalize() #lower() nikita, upper() NIKITA, capitalize() - Nikita
+# print("Tere tulemast! Tervitan Sind ", nimi)
+# print("Tere tulemast! Tervitan Sind "+ nimi)
+# vanus=int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind "+ nimi+" Sa oled ",vanus,"aastat vana")
+# print(f"Tere tulemast! Tervitan sind {nimi} Sa oled {vanus} aastat vana")
 
 
-#Ülessane 2 
-vanus = 18
-eesnimi = "Jaak"
-pikkus = 16.5
-kas_käib_koolis = True
-print(type(vanus))
-print(type(eesnimi))
-print(type(pikkus))
-print(type(kas_käib_koolis))
+# #Ülessane 2 
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus))
+# print(type(eesnimi))
+# print(type(pikkus))
+# print(type(kas_käib_koolis))
 
 
 #Ülessane 3
 kokku=randint(1,1000)
-print=(f"Kokku om {kokku} kommi")
+print(f"Kokku om {kokku} kommi")
 kommi=int(input("Mitu kommi sa tahad? "))
 kokku=kokku-kommi
-print=(f"Jääk om {kokku} kommi ")
+print(f"Jääk om {kokku} kommi ")
+
 
 #Ülessane 4
 print("Läbimõõdu leidmine ")
