489338153bd8d9ef696be621701c13eb3b3ffae3
opilane <opilane@HP-CZC2349HT9>
2024-12-11 15:12:36 +0200

    1

---
+++ _10_практических_заданий_по_теме_Циклы.py
@@ -0,0 +1,19 @@
+﻿#Ül 2 бесконечно, пока не добьётся своего
+
+while True:
+    try:
+        A=int(input("Sisesta A:"))
+        break
+    except:
+        print("On vaja naturaalne arv")
+summa=0
+if A>0:
+    for i in range(1,A+1,1):
+        summa+=i  #summa=summa+i
+        print(f"{i}. samm summa = {summa}")
+print(f"Vastus {summa}")
+
+
+#Ül 
+
+
