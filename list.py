b1fd7d7f42d9a97a7180ee3d4df33b803f1742fa
Nkk15-21 <nkk.sith@gmail.com>
2025-01-15 14:11:51 +0200

    Update list.py

--- list.py
+++ list.py
@@ -4,9 +4,8 @@ glas = ["a", "e", "u", "o", "i", "ü", "õ", "ö", "ä"]
 sogl = "qwrtypsdfghjklzxcvbnm"
 znak = string.punctuation # /*-+;:_,-.?=)(/&%¤"¤%&/()")
 
-g= s = z = t = 0
-
 while True:
+    g = s = z = t = 0
     tekst = input("Sisesta mingi tekst: ").lower()
     if tekst.isdigit():
         break
@@ -14,4 +13,16 @@ while True:
         tekst_list = list(tekst) #для того, чтоюы вывести список из текста
         print(tekst_list)
         for taht in tekst_list:
+            if taht in glas:
+                g+=1
+            elif taht in sogl:
+                s+=1
+            elif taht in znak:
+                z+=1
+            elif taht == " ":
+                t += 1
+    print("Glassnõe: ",g)
+    print("Soglaaanõe: ",s)
+    print("Znaki: ",z)
+    print("Probelõ: ",t)
           
