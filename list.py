3c60b4d018a999d19aa02a262fa6c87cfe13e2a2
Nkk15-21 <nkk.sith@gmail.com>
2025-01-15 14:01:09 +0200

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,17 @@
+import string 
 
+glas = ["a", "e", "u", "o", "i", "ü", "õ", "ö", "ä"]
+sogl = "qwrtypsdfghjklzxcvbnm"
+znak = string.punctuation # /*-+;:_,-.?=)(/&%¤"¤%&/()")
+
+g= s = z = t = 0
+
+while True:
+    tekst = input("Sisesta mingi tekst: ").lower()
+    if tekst.isdigit():
+        break
+    else:
+        tekst_list = list(tekst) #для того, чтоюы вывести список из текста
+        print(tekst_list)
+        for taht in tekst_list:
+          
