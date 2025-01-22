83975b71dd5f0522806f1fff6bbf24d125510d0e
opilane <opilane@HP-CZC2349HT9>
2025-01-22 14:47:02 +0200

    1

--- Def.py
+++ Def.py
@@ -1,1 +1,9 @@
+import random
+from module import *
 
+a=int(input("Введи первое число: "))
+b=int(input("Введи второе число: "))
+c=input("Введи третье число: ")
+
+vastus = summa3(a, b, int(c))
+print(vastus)
